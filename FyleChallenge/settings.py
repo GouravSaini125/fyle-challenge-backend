cf464fbe84b6b92e8aa8fbd889f4c753cb6128e5
Gourav Saini <rahul777singh000@gmail.com>
2020-12-18 11:19:46 +0530

    db url

--- FyleChallenge/settings.py
+++ FyleChallenge/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -139,3 +140,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 django_heroku.settings(locals())
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
